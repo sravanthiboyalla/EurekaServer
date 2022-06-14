11d843f19d8d8fdf4062b84c1ff779e177f1b9bf
Sudhakar Boyalla <boyallasudhakararao@gmail.com>
2022-06-14 10:28:00 +0530

    first Eureka commit

---
+++ src/test/java/com/example/EurekaServerExample/EurekaServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekaServerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
